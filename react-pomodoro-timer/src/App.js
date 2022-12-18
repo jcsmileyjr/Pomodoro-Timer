dc8d94bcab8e964f847465a7f2c0fbc3af89c388
Smiley <jcsmileyjr@gmail.com>
2022-12-18 10:21:10 -0600

    Documentation: add todo list

--- react-pomodoro-timer/src/App.js
+++ react-pomodoro-timer/src/App.js
@@ -1,5 +1,18 @@
 import './App.css';
 
+/**
+ * 
+ * @returns 
+ * 
+ * TODO:
+ * Setup initial basic HTML
+ * Style with initial  CSS
+ * Add basic JavaScript for timer from 15 minutes
+ * Add functionality to start/stop to timer
+ * Add functionality when timer stops, alert message is shown
+ * Add functionality where gear icon is click and user can chage minutes and seconds
+ * HARD PART: Ring around timer should go from red to green. 
+ */
 function App() {
   return (
     <div className="App">
