9ce40cbbad73dbf34dce128e00abdde00544aeab
Smiley <jcsmileyjr@gmail.com>
2022-12-18 10:28:33 -0600

    Setup initial basic UI structure
    No styles and no functionality, visual only

--- react-pomodoro-timer/src/App.js
+++ react-pomodoro-timer/src/App.js
@@ -1,9 +1,10 @@
-import './App.css';
+import "./App.css";
+import Gear from "./assets/gear.svg";
 
 /**
- * 
- * @returns 
- * 
+ *
+ * @returns
+ *
  * TODO:
  * Setup initial basic HTML
  * Style with initial  CSS
@@ -11,12 +12,28 @@ import './App.css';
  * Add functionality to start/stop to timer
  * Add functionality when timer stops, alert message is shown
  * Add functionality where gear icon is click and user can chage minutes and seconds
- * HARD PART: Ring around timer should go from red to green. 
+ * HARD PART: Ring around timer should go from red to green.
  */
 function App() {
   return (
     <div className="App">
-      Hello World
+      <div className="clock--container">
+        <div className="timer--container">
+          <p id="minutes">15</p>
+          <p>:</p>
+          <p id="seconds">00</p>
+        </div>
+        <button className="clock__primaryButton--style" type="button">
+          START
+        </button>
+        <button className="clock__gearButton--style" type="button">
+          <img
+            src={Gear}
+            className="clock__gearIcon--style"
+            alt="Gear icon"
+          ></img>
+        </button>
+      </div>
     </div>
   );
 }
