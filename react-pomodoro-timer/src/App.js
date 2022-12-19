4e68d18da09278a4177b0b3b8976d477ddb63d14
Smiley <jcsmileyjr@gmail.com>
2022-12-18 20:24:54 -0600

    Feature: User can manually update the timer

--- react-pomodoro-timer/src/App.js
+++ react-pomodoro-timer/src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import Gear from "./assets/gear.svg";
+import Check from "./assets/check.svg";
 import React, { useState, useEffect, useCallback } from "react";
 
 /**
@@ -19,6 +20,7 @@ function App() {
   const [counter, setCounter] = useState(false);
   const [minutes, setMinutes] = useState(0);
   const [seconds, setSeconds] = useState(60);
+  const [showGears, setShowGears] = useState(false);
 
   // Decrease the seconds and minute every second.
   const decliningTimer = useCallback(() => {
@@ -52,21 +54,51 @@ function App() {
   return (
     <div className="App">
       <div className="clock--container">
-        <div className="timer--container">
+        <div className={`timer--container ${showGears ? "hide" : ""}`}>
           <p id="minutes">{minutes}</p>
           <p>:</p>
           <p id="seconds">{seconds}</p>
         </div>
-        <button className="clock__primaryButton--style" type="button" onClick={()=> timerControl()}>
+        <button
+          className={`clock__primaryButton--style ${showGears ? "hide" : ""}`}
+          type="button"
+          onClick={() => timerControl()}
+        >
           START
         </button>
-        <button className="clock__gearButton--style" type="button">
+        <button
+          className={`clock__gearButton--style ${showGears ? "hide" : "show"}`}
+          type="button"
+          onClick={() => setShowGears(!showGears)}
+        >
           <img
             src={Gear}
             className="clock__gearIcon--style"
             alt="Gear icon"
           ></img>
         </button>
+        <button
+          className={`clock__gearButton--style ${showGears ? "show" : "hide"}`}
+          type="button"
+          onClick={() => setShowGears(!showGears)}
+        >
+          <img
+            src={Check}
+            className="clock__checkIcon--style"
+            alt="Check icon"
+          ></img>
+        </button>
+        <div className={`gearInputs--container ${showGears ? "show" : "hide"}`}>
+          <label htmlFor="updateMinute">Minutes</label>
+          <input
+            id="updateMinute"
+            type="number"
+            value={minutes}
+            onChange={(event) => setMinutes(event.target.value)}
+          />
+          <label htmlFor="updateSeconds">Seconds</label>
+          <input id="updateSeconds" type="number" />
+        </div>
       </div>
     </div>
   );
